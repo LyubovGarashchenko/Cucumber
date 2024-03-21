e30cef788883ac4853be376f22bb02547e308661
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2024-03-21 12:25:06 +0300

    $ not active

---
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -0,0 +1,44 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+
+public class DashboardPage {
+    private final SelenideElement title = $("[data-test-id=dashboard]");
+    private final String balanceStart = ", баланс: ";
+    private final String balanceFinish = " р.";
+
+    private final ElementsCollection cards = $$(".list__item div");
+
+    public DashboardPage() {
+        title.shouldBe(visible);
+    }
+
+    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+        var text = cards.findBy(Condition.text(cardInfo.getCardNumber().substring(15)))
+                .getText();
+        return extractBalance(text);
+    }
+
+    public int getCardBalance(int index) {
+        var text = cards.get(index).getText();
+        return extractBalance(text);
+    }
+
+    public MoneyTransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestId())).$("button").click();
+        return new MoneyTransferPage();
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
